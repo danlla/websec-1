943e644529d04498053de0a6b63808aa5b3a836e
danila <danila_glazkov@mail.ru>
2022-09-02 14:15:04 +0400

    added changing color, zero check

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
 function buttonClicked() {
     console.log('button clicked');
+    document.getElementById('lhs').style.background = 'lightcyan';
+    document.getElementById('rhs').style.background = 'lightcyan';
 
     const lhs = document.getElementById('lhs').value;
-    if (/\D/.test(lhs)){
-        document.getElementById('textarea').innerText += 'left operator not valid';
+    if (/\D/.test(lhs)) {
+        document.getElementById('results').value += 'left operator not valid\n';
+        document.getElementById('lhs').style.background = 'red';
         return;
     }
     const rhs = document.getElementById('rhs').value;
-    if (/\D/.test(rhs)){
-        document.getElementById('textarea').innerText += 'right operator not valid';
+    if (/\D/.test(rhs)) {
+        document.getElementById('results').value += 'right operator not valid\n';
+        document.getElementById('rhs').style.background = 'red';
         return;
     }
 
@@ -25,9 +29,14 @@ function buttonClicked() {
             res = parseFloat(lhs) * parseFloat(rhs);
             break;
         case '/':
+            if(rhs==0.0)
+            {
+                document.getElementById('results').value += 'right operator is zero\n';
+                document.getElementById('rhs').style.background = 'red';
+                return;
+            }
             res = parseFloat(lhs) / parseFloat(rhs);
             break;
     }
-
-    document.getElementById('textarea').innerText = res;
+    document.getElementById('results').value += res + '\n';
 }
