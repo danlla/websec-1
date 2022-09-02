933c57d74b7160d261eff350af5fa253efc4f3c7
danila <danila_glazkov@mail.ru>
2022-09-02 14:59:50 +0400

    added disabling button when validation errors exist

--- src/script.js
+++ src/script.js
@@ -30,17 +30,20 @@ function buttonClicked() {
 function inputChanged(){
     document.getElementById('lhs').style.background = 'lightcyan';
     document.getElementById('rhs').style.background = 'lightcyan';
+    document.getElementById('button').disabled=false;
 
     const lhs = document.getElementById('lhs').value;
     if (isNaN(lhs)) {
         document.getElementById('results').value += 'left operator not valid\n';
         document.getElementById('lhs').style.background = 'red';
+        document.getElementById('button').disabled=true;
         return;
     }
     const rhs = document.getElementById('rhs').value;
     if (isNaN(rhs)) {
         document.getElementById('results').value += 'right operator not valid\n';
         document.getElementById('rhs').style.background = 'red';
+        document.getElementById('button').disabled=true;
         return;
     }
 }
