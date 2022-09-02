a88723def7fb5442ec33641036a9a98bc78fc298
danila <danila_glazkov@mail.ru>
2022-09-02 14:43:26 +0400

    added validating when numbers are entered, changed comparing with zero

--- src/script.js
+++ src/script.js
@@ -1,20 +1,6 @@
 function buttonClicked() {
-    console.log('button clicked');
-    document.getElementById('lhs').style.background = 'lightcyan';
-    document.getElementById('rhs').style.background = 'lightcyan';
-
     const lhs = document.getElementById('lhs').value;
-    if (/\D/.test(lhs)) {
-        document.getElementById('results').value += 'left operator not valid\n';
-        document.getElementById('lhs').style.background = 'red';
-        return;
-    }
     const rhs = document.getElementById('rhs').value;
-    if (/\D/.test(rhs)) {
-        document.getElementById('results').value += 'right operator not valid\n';
-        document.getElementById('rhs').style.background = 'red';
-        return;
-    }
 
     const operation = document.getElementById('select').value;
     let res;
@@ -29,7 +15,7 @@ function buttonClicked() {
             res = parseFloat(lhs) * parseFloat(rhs);
             break;
         case '/':
-            if(rhs==0.0)
+            if((rhs-0.0)<Number.EPSILON)
             {
                 document.getElementById('results').value += 'right operator is zero\n';
                 document.getElementById('rhs').style.background = 'red';
@@ -39,4 +25,22 @@ function buttonClicked() {
             break;
     }
     document.getElementById('results').value += res + '\n';
+}
+
+function inputChanged(){
+    document.getElementById('lhs').style.background = 'lightcyan';
+    document.getElementById('rhs').style.background = 'lightcyan';
+
+    const lhs = document.getElementById('lhs').value;
+    if (isNaN(lhs)) {
+        document.getElementById('results').value += 'left operator not valid\n';
+        document.getElementById('lhs').style.background = 'red';
+        return;
+    }
+    const rhs = document.getElementById('rhs').value;
+    if (isNaN(rhs)) {
+        document.getElementById('results').value += 'right operator not valid\n';
+        document.getElementById('rhs').style.background = 'red';
+        return;
+    }
 }
